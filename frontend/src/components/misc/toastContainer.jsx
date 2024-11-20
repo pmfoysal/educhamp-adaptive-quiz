e11ce6d29e34ecdc0ae50304b02879f5fe89ce39
pmfoysal <pmfoysal@gmail.com>
2024-11-20 13:15:17 +0600

    ToastContainer Component Added

---
+++ frontend/src/components/misc/toastContainer.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+import Toaster from 'react-hot-toast';
+
+export default function ToastContainer({ children }) {
+	return (
+		<Fragment>
+			{children}
+			<Toaster position='top-center' reverseOrder={false} />
+		</Fragment>
+	);
+}
