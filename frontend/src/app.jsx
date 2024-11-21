8695466d608b353854be514e8b284542263044b2
pmfoysal <pmfoysal@gmail.com>
2024-11-21 22:20:19 +0600

    Analytics Page Component Added

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -8,6 +8,7 @@ const ResultsPage = lazy(() => import('./pages/resultsPage'));
 const HomeLayout = lazy(() => import('./layouts/homeLayout'));
 const AuthLayout = lazy(() => import('./layouts/authLayout'));
 const NotFoundPage = lazy(() => import('./pages/notFoundPage'));
+const AnalyticsPage = lazy(() => import('./pages/analyticsPage'));
 const PageLoader = lazy(() => import('./components/loaders/pageLoader'));
 
 export default function App() {
@@ -18,6 +19,7 @@ export default function App() {
 					<Route index element={<QuizzesPage />} />
 					<Route path='quizzes' element={<QuizzesPage />} />
 					<Route path='results' element={<ResultsPage />} />
+					<Route path='analytics' element={<AnalyticsPage />} />
 				</Route>
 				<Route path='/' element={<AuthLayout />}>
 					<Route path='signin' element={<SigninPage />} />
