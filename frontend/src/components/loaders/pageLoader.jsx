eaed1b3a5ba9e251d7dc28f2000ae7ba534de711
pmfoysal <pmfoysal@gmail.com>
2024-11-27 15:44:10 +0600

    Page Loader Component Updated

--- frontend/src/components/loaders/pageLoader.jsx
+++ frontend/src/components/loaders/pageLoader.jsx
@@ -1,3 +1,15 @@
+import styles from './pageLoader.module.css';
+import { Hearts } from 'react-loader-spinner';
+
 export default function PageLoader() {
-	return <div>PageLoader</div>;
+	return (
+		<main className={styles.page}>
+			<article className={styles.content}>
+				<Hearts height='100' width='100' color='#ef4444' visible={true} />
+				<h1>
+					Adapt<span>Quiz</span>
+				</h1>
+			</article>
+		</main>
+	);
 }
