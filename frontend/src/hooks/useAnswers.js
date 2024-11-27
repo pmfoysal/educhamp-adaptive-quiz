2f47ca9aba36c0db63e802a17a9181998ded8577
pmfoysal <pmfoysal@gmail.com>
2024-11-27 13:07:11 +0600

    Answer API Hooks Created

---
+++ frontend/src/hooks/useAnswers.js
@@ -0,0 +1,32 @@
+import api from '../utilities/api';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+export function useHasAnswer(id) {
+	return useQuery({
+		queryKey: ['hasAnswer', id],
+		queryFn: async () => {
+			const result = await api.get(`/quiz/answer/user/${id}`);
+			return result.data;
+		},
+	});
+}
+
+export function useAnswerReport(id) {
+	return useQuery({
+		queryKey: ['answerReport', id],
+		queryFn: async () => {
+			const result = await api.get(`/quiz/answer/reports/${id}`);
+			return result.data;
+		},
+	});
+}
+
+export function useSubmitAnswer() {
+	return useMutation({
+		mutationKey: ['submitAnswer'],
+		mutationFn: async data => {
+			const result = await api.post('/quiz/answer', data);
+			return result.data;
+		},
+	});
+}
