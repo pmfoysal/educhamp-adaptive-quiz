3376f45a4aeabcffb57399938ec2c99743b2b7dd
pmfoysal <pmfoysal@gmail.com>
2024-11-28 15:56:52 +0600

    Route Middlewares Added

--- backend/features/answers/answer.route.js
+++ backend/features/answers/answer.route.js
@@ -1,9 +1,13 @@
 const api = require('express').Router();
 const controllers = require('./answer.controller');
+const middlewares = require('../auth/auth.middleware');
 
-api.route('/answer').post(controllers.addOne);
-api.get('/answer/user', controllers.getOneByUser);
-api.get('/answer/reports', controllers.getReports);
-api.route('/answer/:id').get(controllers.getOne).patch(controllers.editOne).delete(controllers.deleteOne);
+api.route('/answer').post(middlewares.authChecker, controllers.addOne);
+api.get('/answer/user', middlewares.authChecker, controllers.getOneByUser);
+api.get('/answer/reports', middlewares.authChecker, controllers.getReports);
+api.route('/answer/:id')
+	.get(middlewares.authChecker, controllers.getOne)
+	.patch(middlewares.authChecker, controllers.editOne)
+	.delete(middlewares.authChecker, controllers.deleteOne);
 
 module.exports = api;
--- backend/features/questions/question.route.js
+++ backend/features/questions/question.route.js
@@ -1,10 +1,17 @@
 const api = require('express').Router();
 const controllers = require('./question.controller');
+const middlewares = require('../auth/auth.middleware');
 
-api.route('/question').post(controllers.addOne);
-api.get('/questions/total', controllers.getTotal);
-api.post('/questions/next', controllers.getNextOne);
-api.route('/questions').post(controllers.addMany).get(controllers.getMany).delete(controllers.deleteMany);
-api.route('/question/:id').get(controllers.getOne).patch(controllers.editOne).delete(controllers.deleteOne);
+api.route('/question').post(middlewares.authChecker, controllers.addOne);
+api.get('/questions/total', middlewares.authChecker, controllers.getTotal);
+api.post('/questions/next', middlewares.authChecker, controllers.getNextOne);
+api.route('/questions')
+	.post(middlewares.authChecker, controllers.addMany)
+	.get(middlewares.authChecker, controllers.getMany)
+	.delete(middlewares.authChecker, controllers.deleteMany);
+api.route('/question/:id')
+	.get(middlewares.authChecker, controllers.getOne)
+	.patch(middlewares.authChecker, controllers.editOne)
+	.delete(middlewares.authChecker, controllers.deleteOne);
 
 module.exports = api;
