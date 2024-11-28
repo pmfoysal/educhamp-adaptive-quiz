7f63d57ed90dd6c88471d9a0f264bd1e74077fdb
pmfoysal <pmfoysal@gmail.com>
2024-11-28 15:58:54 +0600

    Root API Response Added

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,19 @@ const questionRoutes = require('./features/questions/question.route');
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: '*' }));
+const PORT = process.env.PORT || 5000;
 
 app.use('/api/auth', authRoutes);
 app.use('/api/quiz', answerRoutes);
 app.use('/api/quiz', questionRoutes);
+
+app.get('/', (req, res) => {
+	res.status(200).send(`Server running on port ${PORT}`);
+});
 
 mongoose
 	.connect(process.env.MONGODB_URL, { serverApi: { version: '1', strict: true, deprecationErrors: true } })
 	.then(() => console.log('MongoDB Connected!'.green))
 	.catch(err => console.error('MongoDB Connection Failed!'.red, err));
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`.blue));
